098672be330a51650e81c6f85445dd58f0cf37c6
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-14 18:40:10 -0500

    Ruta de login

--- login/views.py
+++ login/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+  return HttpResponse('login')
