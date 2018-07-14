8f9c2366a7da7a2434770605a979491a58e6cd1b
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-14 18:33:25 -0500

    Archivo de constantes

---
+++ main/constants.py
@@ -0,0 +1,32 @@
+constants = {
+  'base_url' : 'http://localhost:8080/',
+  'static_url' : 'http://localhost:8080/static/',
+  'csrf' : {
+    'key': 'csrfmiddlewaretoken',
+    'secret': '4n5)f$(xj7lxneya=2a=p9w!-jf(vwt0i3sge)#gh16&h&c+&7'
+  },
+  'ambiente_static' : 'desarrollo',
+  'ambiente_session' : 'activo',
+  'ambiente_csrf' : 'activo',
+  'key' : 'Fdp6CVxpri1ga8kH',
+  'servicios' : {
+    'accesos' : {
+      'url': 'http://localhost:5000/',
+      'key': 'csrf_val',
+      'secret': 'PKBcauXg6sTXz7Ddlty0nejVgoUodXL89KNxcrfwkEme0Huqtj6jjt4fP7v2uF4L',
+    },
+    'agricultores' : 'http://localhost:3012/',
+    'ubicaciones' : 'http://localhost:3011/',
+    'archivos' : 'http://192.168.1.51:3031/',
+    'ftp' : {
+      'dominio' : '192.168.1.51',
+      'puerto' : 22,
+      'usuario' : 'pepe',
+      'contrasenia' : 'kiki123',
+      'ruta' : '/home/pepe/Documentos/py2/archivos/static/',
+      'public' : 'http://192.168.1.51:3031/'
+     },
+    'estaciones' : 'http://localhost:3025/',
+    'sensores' : 'http://localhost:3035/',
+  }
+}
