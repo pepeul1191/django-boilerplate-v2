f4f1fd4dcf04ea969f8b8ad28bf094a680b64f49
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-15 19:23:16 -0500

    Home con handlebars y backbone

--- home/helpers.py
+++ home/helpers.py
@@ -6,8 +6,10 @@ def index_css():
       'bower_components/bootstrap/dist/css/bootstrap.min',
       'bower_components/font-awesome/css/font-awesome.min',
       'bower_components/swp-backbone/assets/css/constants',
-      'bower_components/swp-backbone/assets/css/login',
-      'assets/css/home',
+      'bower_components/swp-backbone/assets/css/dashboard',
+      'bower_components/swp-backbone/assets/css/table',
+      'assets/css/constants',
+      'assets/css/styles',
     ],
     'produccion': ['dist/home.min'],
   }
@@ -15,7 +17,27 @@ def index_css():
 
 def index_js():
   switcher = {
-    'desarrollo': [],
+    'desarrollo': [
+      'bower_components/jquery/dist/jquery.min',
+      'bower_components/bootstrap/dist/js/bootstrap.min',
+      'bower_components/underscore/underscore-min',
+      'bower_components/backbone/backbone-min',
+      'bower_components/handlebars/handlebars.min',
+      'bower_components/swp-backbone/layouts/application',
+      'bower_components/swp-backbone/views/table',
+      'bower_components/swp-backbone/views/modal',
+      'models/ubicaciones/departamento',
+      'models/ubicaciones/provincia',
+      'collections/ubicaciones/departamento_collection',
+      'collections/ubicaciones/provincia_collection',
+      'data/ubicaciones/tabla_distrito',
+      'data/ubicaciones/tabla_provincia',
+      'data/ubicaciones/tabla_departamento',
+      'views/ubicaciones/tabla_provincia_view',
+      'views/ubicaciones/tabla_departamento_view',
+      'views/ubicaciones/ubicacion_view',
+      'routes/ubicaciones',
+    ],
     'produccion': [],
   }
   return switcher.get(constants['ambiente_static'])
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import requests
+import json
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from error.views import methodNotAllow
@@ -13,6 +14,24 @@ def index(request):
       'mensaje': '',
       'csss': index_css(),
       'jss': index_js(),
+      'menu' : json.dumps([
+		{'url' : 'accesos/', 'nombre' : 'Accesos'},
+        {'url' : 'maestros/', 'nombre' : 'Maestros'},
+    	{'url' : 'agricultores/', 'nombre' : 'Agricultores'},
+    	{'url' : 'estaciones/', 'nombre' : 'Estaciones'},
+      ]),
+      'items' : json.dumps([
+    	{"subtitulo":"Opciones", "items":
+    	  [
+    		{"item":"Gestión de Sistemas","url":"accesos/#/sistema"},
+    		{"item":"Gestión de Usuarios","url":"accesos/#/usuario"}
+    	  ]
+    	},
+      ]),
+      'data' : json.dumps({
+        'titulo_pagina' : 'Gestión Accesos',
+        'modulo' : 'Accesos'
+      }),
     }
     return render(request, 'home/index.html', locals)
   else:
